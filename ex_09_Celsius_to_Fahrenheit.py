56a8b6b40a1499a1056746f6d4ea4e3dfdee5f09
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:42:59 +0530

    Celsius to Fahrenheit

---
+++ ex_09_Celsius_to_Fahrenheit.py
@@ -0,0 +1,9 @@
+'''
+Convertion of Celsius to Fahrenheit is 
+((c * 9)/5) + 32 = f
+where c is temp in celsius
+and f is temp in fahrenheit
+'''
+c=float(input('Enter temp in degree celsius here: ', ))
+f=((c * 9)/5) + 32
+print("Convertion to Fahrenheit is : ", f)
