9bb73bddcc69a8f9a7a1a202a4d59884579166bf
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:40:22 +0530

    Square root

---
+++ ex_03_Squareroot.py
@@ -0,0 +1,16 @@
+#first type (only for positive numbers)
+'''
+a=4
+a_sqrt=a**0.5
+print((a,a_sqrt))
+'''
+
+#second type (for complex numbers + positive numbers)
+
+import cmath
+num=16
+
+#user define data
+#num=float(input("Enter number here :",))
+num_sqrt=cmath.sqrt(num)
+print(num_sqrt)
