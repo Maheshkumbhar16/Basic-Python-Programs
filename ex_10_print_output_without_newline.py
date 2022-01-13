0376bc871445892f9e2a06416c60814b31ced274
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:43:35 +0530

    Print output without newline

---
+++ ex_10_print_output_without_newline.py
@@ -0,0 +1,9 @@
+#print each statement on new line
+print("Hello")
+print("World!")
+print() #Enter New line in between
+print("World!")
+
+#print both statement on single line
+print("Hello ",end="")
+print("World!")
