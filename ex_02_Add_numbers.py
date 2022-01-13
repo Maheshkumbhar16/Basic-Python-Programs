5ae8b98695567c6e0716ec1a4c836c72c69ef6ff
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:39:50 +0530

    Add numbers

---
+++ ex_02_Add_numbers.py
@@ -0,0 +1,23 @@
+#first type
+'''
+a=1
+b=2
+print("Sum of a+b is ", a+b)
+'''
+
+#second type
+'''
+def sum(marks):
+    s=marks[0]+marks[1]
+    return s
+marks1=[1,2]
+sum1=sum(marks1)
+print(sum1)
+'''
+
+#third type(user define number)
+a=input("Enter first number: ")
+a=int(a)
+b=input("Enter second number: ")
+b=int(b)
+print("Sum of numbers is:", a+b)
