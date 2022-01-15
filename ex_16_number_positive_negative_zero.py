563cb4a52dd7f01be7be93ab67ee578c99ef9276
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-15 13:49:16 +0530

    Number is positive/negative or zero

---
+++ ex_16_number_positive_negative_zero.py
@@ -0,0 +1,8 @@
+a=float(input("Enter number here : ",))
+if a>0:
+    print ("Enter number is POSITIVE")
+
+elif a==0 :
+    print ("Enter number is ZERO")
+else:
+    print ("Enter number is NEGATIVE")
