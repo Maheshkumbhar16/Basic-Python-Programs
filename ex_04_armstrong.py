b753ae9c4165152813392568d4bace5bf1cf8698
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-15 13:51:16 +0530

    Armstrong

---
+++ ex_04_armstrong.py
@@ -0,0 +1,10 @@
+'''
+a positive int is called as Armstrong of order n if
+abcd = a^n + b^n + c^n + d^n + ........
+example:
+in case of an armstrong number of 3 digits, the sum of cubes of each digit is equal to the number itself
+153 = 1*1*1 + 5*5*5 + 3*3*3
+total is 153
+'''
+a=int(input("Enter the number here:"))
+b=int(input("Enter the digits here :"))
